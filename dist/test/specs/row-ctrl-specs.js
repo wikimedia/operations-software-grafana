196550e5dbe8081a42017e2aeec7b797d8446a5e
Ori Livneh <ori@wikimedia.org>
2014-10-02 14:08:07 -0700

    Build a301c96c9d

---
+++ dist/test/specs/row-ctrl-specs.js
@@ -0,0 +1,4 @@
+/*! grafana - v1.9.0 - 2014-11-10
+ * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
+
+define(["./helpers","controllers/row"],function(a){describe("RowCtrl",function(){var b=new a.ControllerTestContext;beforeEach(module("grafana.controllers")),beforeEach(b.providePhase()),beforeEach(b.createControllerPhase("RowCtrl"))})});
